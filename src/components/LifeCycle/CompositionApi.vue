fb558702536ba4a198f1df551b22c31c1ed7e2c3
Jasom Rocha <jasom@qstione.com.br>
2026-06-08 00:07:14 -0300

    LifeCycle Hooks: Entendendo o uso de Hooks nos componentes vue, em compositions e options Api

---
+++ src/components/LifeCycle/CompositionApi.vue
@@ -0,0 +1,47 @@
+<template>
+<h1>Composition API</h1>
+  <p>O contador está no valor: {{ counter }}</p>
+  <button @click="counter++">Somar</button>
+</template>
+
+<script setup>
+import {
+  onBeforeMount,
+  onBeforeUnmount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref
+} from 'vue';
+
+const counter = ref(0);
+
+onBeforeMount(() => {
+  console.log('1 - Before Mounted');
+})
+
+onMounted(() => {
+  console.log('2 - on Mounted');
+})
+
+onBeforeUpdate(() => {
+  console.log('3 - Before Update');
+})
+
+onUpdated(() => {
+  console.log('4 - Updated');
+})
+
+onBeforeUnmount(() => {
+  console.log('5 - Before Unmounted');
+})
+
+onUnmounted(() => {
+  console.log('6 - on Unmounted');
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/LifeCycle/OpitionsApi.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>Options Api</h1>
+  <p>Contador: {{ counter }} </p>
+  <button @click="counter++">Somar</button>
+</template>
+
+<script>
+export default {
+  name: "OpitionsApi",
+  data(){
+    return {
+      counter: 0
+    }
+  },
+  beforeCreate(){
+    console.log("1 - before Create");
+  },
+  created(){
+    console.log("2 - Created");
+  },
+  beforeMount(){
+    console.log("3 - before Mount");
+  },
+  mounted(){
+    console.log("4 - Mounted");
+  },
+  beforeUpdate(){
+    console.log("5 - before Update");
+  },
+  updated(){
+    console.log("6 - Updated");
+  },
+  beforeDestroy(){
+    console.log("7 - before destroy");
+  },
+  destroyed(){
+    console.log("8 - Destroyed");
+  }
+}
+</script>
+<style scoped>
+
+</style>
