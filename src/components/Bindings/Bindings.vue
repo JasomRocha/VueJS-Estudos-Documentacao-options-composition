ddd4bc4a49999b70df548e2ac47e525f6244c9e8
Jasom Rocha <jasom@qstione.com.br>
2026-06-08 00:05:06 -0300

    Bindings: Entendendo o uso de Bindings (v-bind ou :) Ref compositions e options Api

---
+++ src/components/Bindings/Bindings.vue
@@ -0,0 +1,36 @@
+<template>
+  <h1> Bindings </h1>
+
+  <label>Nome: </label>
+  <input :type="input.type" :name="input.name">
+  <label>Senha: </label>
+  <input v-bind:type="pass.type" v-bind:name="pass.name"></input>
+  <input type="button" value="Mostrar senha" @click="showHidePassword" />
+</template>
+
+<script setup>
+import {reactive} from "vue";
+
+const input = reactive({
+  name: "name",
+  type: "text",
+  placeholder: "Digite seu nome aqui",
+})
+
+const pass = reactive({
+  name: "password",
+  type: "password",
+})
+
+const showHidePassword = function(){
+  if(pass.type === "text"){
+    pass.type = "password";
+  } else {
+    pass.type = "text";
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
