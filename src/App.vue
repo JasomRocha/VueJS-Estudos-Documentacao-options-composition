c83185c3c2ebf3a0c5f0e78d19e5eb49a6cb0084
Jasom <jasom@qstione.com.br>
2026-06-09 09:10:21 -0300

    Register: Quando temos aplicações com OptionsAPI temos outras maneiras de registrar componentes, comum em aplicações legadas

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
-<script setup>
+<script>
 import VPre from "./components/Directives/VPre.vue";
 import VOnce from "./components/Directives/VOnce.vue";
 import VMemo from "./components/Directives/VMemo.vue";
+
+// Quando retiramos o setup do script, ele importa como se fosse options API
+// A maneira como ele importa é diferente da maneira como é feito no compositions API
+// import {defineComponent} from "vue";
+// import Options from "./components/Register/Options.vue";
+//
+// export default defineComponent({
+//   components: {Options}
+// })
+
+// Você pode dar um ALIAS para o seu componente se julgar necessário (aplicação legada faz isso)
+import {defineComponent} from "vue";
+import Options from "./components/Register/Options.vue";
+
+export default defineComponent({
+  components: {
+    MeuComponenteA: Options
+  }
+
+})
+
 </script>
+
+
 <template>
 <!--  <options-api />-->
 <!--  <RefExample />-->
@@ -13,27 +36,29 @@ import VMemo from "./components/Directives/VMemo.vue";
 <!--  <Bindings/>-->
 <!--  <EventHandling />-->
 <!--<Form />-->
-  <!--  <VIf />-->
-<!--  <VShow />-->
-<!--  <VText />-->
-<!--  <VFor />-->
+<!--  <VIf />-->
+<!-- <VShow />-->
+<!-- <VText />-->
+<!-- <VFor />-->
 
 
-<!--  <VSlot>Editar usuário!</VSlot>-->
-<!--  <Card>-->
-<!--    <template v-slot:header>-->
-<!--      <h1>Título do meu card</h1>-->
-<!--    </template>-->
+<!-- <VSlot>Editar usuário!</VSlot>-->
+<!-- <Card>-->
+<!-- <template v-slot:header>-->
+<!-- <h1>Título do meu card</h1>-->
+<!--</template>-->
 
-<!--    <template #content>-->
-<!--      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, saepe!</h1>-->
-<!--    </template>-->
+<!--<template #content>-->
+<!-- <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, saepe!</h1>-->
+<!--</template>-->
 <!--  </Card>-->
 
 <!--</template>-->
 
 <!--  <VPre />-->
 
-  <VOnce />
-  <VMemo />
+<!--<VOnce />-->
+<!--<VMemo />-->
+
+<Options />
 </template>
---
+++ src/components/Register/Composition.vue
@@ -0,0 +1,4 @@
+<template>
+<h1> Composition register </h1>
+</template>
+
