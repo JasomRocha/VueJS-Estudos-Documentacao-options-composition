23e39094f36a1131a7b0b9b1aff113e3b844a0f9
Jasom <jasom@qstione.com.br>
2026-06-12 16:06:06 -0300

    Register: Quando temos aplicações com OptionsAPI temos outras maneiras de registrar componentes, comum em aplicações legadas

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,18 @@
+<template>
+  <CompositionCard
+    title="Esse é o meu título"
+    :body="'Esse é o meu card'"
+    :access=123
+    :status=true
+    format="company" />
+</template>
+
 <script>
-import VPre from "./components/Directives/VPre.vue";
-import VOnce from "./components/Directives/VOnce.vue";
-import VMemo from "./components/Directives/VMemo.vue";
-
-// Quando retiramos o setup do script, ele importa como se fosse options API
-// A maneira como ele importa é diferente da maneira como é feito no compositions API
-// import {defineComponent} from "vue";
-// import Options from "./components/Register/Options.vue";
-//
-// export default defineComponent({
-//   components: {Options}
-// })
-
-// Você pode dar um ALIAS para o seu componente se julgar necessário (aplicação legada faz isso)
 import {defineComponent} from "vue";
-import Options from "./components/Register/Options.vue";
+import CompositionCard from "./components/Props/CompositionCard.vue";
 
 export default defineComponent({
-  components: {
-    MeuComponenteA: Options
-  }
-
+  components: {CompositionCard}
 })
 
 </script>
-
-
-<template>
-<!--  <options-api />-->
-<!--  <RefExample />-->
-<!--  <ReactiveExample />-->
-<!--<v-model-example />-->
-<!--  <CompositionAPI />-->
-<!--  <TemplateRef/>-->
-<!--  <Bindings/>-->
-<!--  <EventHandling />-->
-<!--<Form />-->
-<!--  <VIf />-->
-<!-- <VShow />-->
-<!-- <VText />-->
-<!-- <VFor />-->
-
-
-<!-- <VSlot>Editar usuário!</VSlot>-->
-<!-- <Card>-->
-<!-- <template v-slot:header>-->
-<!-- <h1>Título do meu card</h1>-->
-<!--</template>-->
-
-<!--<template #content>-->
-<!-- <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, saepe!</h1>-->
-<!--</template>-->
-<!--  </Card>-->
-
-<!--</template>-->
-
-<!--  <VPre />-->
-
-<!--<VOnce />-->
-<!--<VMemo />-->
-
-<Options />
-</template>
---
+++ src/components/Props/CompositionCard.vue
@@ -0,0 +1,83 @@
+
+<template>
+ <h1> Título do meu card: {{ props.title}}</h1>
+  <p> Saída do form.title: {{ form.title }}</p>
+  <input type="text" v-model="form.title" />
+
+  <p> Corpo do meu card: {{ props.body }}</p>
+
+  <p> Quantidade de acessos: {{ props.access }}</p>
+
+  <p> status: {{ props.status }} </p>
+</template>
+
+<script setup>
+// Uso mais básico das props (Não recomendada)
+// const props = defineProps([
+//     'title', 'body', 'access', 'status'
+// ])
+
+// const props = defineProps({
+//   title: String,
+//   body: String,
+//   access: Number,
+//   status: Boolean
+// })
+
+// Além de declarar as props já conseguimos definir valores padrão e gerar validações
+// Essa forma é a mais utilizada
+// const props = defineProps({
+//   title: {
+//     type: String,
+//     required: true
+//   },
+//   body:{
+//     type: String,
+//     default: 'Valor Padrão'
+//   },
+//   access: {
+//     type: Number || String, // pode ser assim também [Number, String]
+//     default: 0
+//   },
+//   status: {
+//     type: Boolean,
+//     required: true
+//   }
+// })
+
+import {reactive} from "vue";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  body:{
+    type: String,
+    default: 'Valor Padrão'
+  },
+  access: {
+    type: Number || String, // pode ser assim também [Number, String]
+    default: 0
+  },
+  status: {
+    type: Boolean,
+    required: true
+  },
+  format: {
+    type: String,
+    required: true,
+    validator(value){
+      return ['post', 'user'].includes(value)
+    }
+  }
+})
+
+const form = reactive({
+  title: props.title
+})
+</script>
+
+<style scoped>
+
+</style>
